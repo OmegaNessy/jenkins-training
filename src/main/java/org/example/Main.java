11fb742262d8b1b3431a23110586545c491f13a6
Andrei_Kavalikhin <Andrei.Kavalikhin@stepstone.com>
2024-06-05 17:42:18 +0400

    feat jenkins file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world change222222232!");
+        System.out.println("Hello world change2222222232!");
     }
 }
