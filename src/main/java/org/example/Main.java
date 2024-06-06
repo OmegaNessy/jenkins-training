3101c1c609247f78471360c93fd46720a52445e1
OmegaNessy <kavalikhin@gmail.com>
2024-06-06 18:53:15 +0400

    change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world chan!");
+        System.out.println("Hello world 2chan!");
     }
 }
