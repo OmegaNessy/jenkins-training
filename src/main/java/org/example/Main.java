ac6097a82c5c6259b8f72da6a90aa7ad5fc99adb
OmegaNessy <kavalikhin@gmail.com>
2024-06-07 19:54:13 +0400

    change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         while(true){
-            System.out.println("Hello world loop!" + Instant.now());
+            System.out.println("Hello world loop2!" + Instant.now());
             Thread.sleep(1000);
         }
     }
